1f629a9e33ea088b07bdfe728b642622142a72b8
Soham Choudhury - s0c0kug <Soham.Choudhury@walmart.com>
2024-10-22 18:32:21 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='softeam_common_utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies of your package
+    ],
+)
