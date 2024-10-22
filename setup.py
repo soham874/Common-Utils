fdbb4a980708ae3a99f956e313367288d25c2dbe
Soham Choudhury - s0c0kug <Soham.Choudhury@walmart.com>
2024-10-22 19:44:15 +0530

    update setup

--- setup.py
+++ setup.py
@@ -7,4 +7,17 @@ setup(
     install_requires=[
         'redis'
     ],
+    include_package_data=True,  # Ensures non-Python files (like README.MD) are included
+    description='A utility package for common functions in the Softeam project',
+    long_description=open('README.MD').read(),
+    long_description_content_type='text/markdown',
+    author='Soham Choudhury',
+    author_email='soham874@gmail.com',
+    url='https://github.com/soham874/Common-Utils',  # Replace with actual URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Update if using a different license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Adjust based on your Python version requirements
 )
