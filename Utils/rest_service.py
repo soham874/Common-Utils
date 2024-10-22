27bd595be834cc17079f7f4f7bffa26ca766cdaa
Soham Choudhury - s0c0kug <Soham.Choudhury@walmart.com>
2024-10-22 19:01:13 +0530

    Add common files

---
+++ Utils/rest_service.py
@@ -0,0 +1,28 @@
+from common_config.log_config import get_logger
+import requests
+
+log = get_logger(__name__)
+
+def execute_rest_call(
+        url,
+        params = {},
+        headers = {},
+        payload = {},
+        method = "GET"):
+    
+    default_headers = {
+        "User-Agent": "insomnium/0.2.3-a"
+    }
+
+    response = requests.request(
+        method = method,
+        headers = default_headers | headers ,
+        data = payload,
+        params = params,
+        url=url
+    )
+            
+    if not 200 <= response.status_code < 400:
+        raise Exception (f"Failed to execute {method} call to URL {url} with status code {response.status_code} and response {response.text}")
+    
+    return response.text
