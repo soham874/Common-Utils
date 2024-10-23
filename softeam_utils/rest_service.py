7e222eeb5530324bf8e459cecc93fc85ea3c749d
Soham Choudhury - s0c0kug <Soham.Choudhury@walmart.com>
2024-10-23 22:39:40 +0530

    Bug fixes

--- softeam_utils/rest_service.py
+++ softeam_utils/rest_service.py
@@ -1,5 +1,6 @@
-from common_config.log_config import get_logger
+from softeam_common_config.log_config import get_logger
 import requests
+from urllib.parse import urlencode, urljoin
 
 log = get_logger(__name__)
 
@@ -25,4 +26,7 @@ def execute_rest_call(
     if not 200 <= response.status_code < 400:
         raise Exception (f"Failed to execute {method} call to URL {url} with status code {response.status_code} and response {response.text}")
     
-    return response.text
+    return response
+
+def get_param_parsed_url(baseURL,queryParams):
+    return urljoin(baseURL, '?' + urlencode(queryParams))
