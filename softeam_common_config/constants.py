0755b7c5753c29eee01bd2a33bfa4c5ec75b4041
Soham Choudhury <73630164+soham874@users.noreply.github.com>
2024-12-11 23:09:50 +0530

    Zerodha auth headers

--- softeam_common_config/constants.py
+++ softeam_common_config/constants.py
@@ -3,6 +3,7 @@ from pathlib import Path
 BASEDIR=Path(__file__).parent.parent
 
 UPSTOX_AUTH_HEADERS='upstox_headers'
+ZERODHA_AUTH_HEADERS='zerodha_auth_headers'
 
 # Analysis strategies
 SUPPORTED_ANALYSIS_STRATEGIES = [
